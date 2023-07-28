0a6696f95c897dc485c067adb12df9e7299567ab
Victor Diaz <master_v12@hotmail.com>
2023-07-27 21:38:15 -0700

    added styling and toggle collapse to budget item components

--- src/app/components/item-list-components/budget-item/budget-item.component.ts
+++ src/app/components/item-list-components/budget-item/budget-item.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BudgetItemDto } from '../budget-item/budget-item-dto';
 
 @Component({
-  selector: 'tr[app-budget-item]',
+  selector: 'app-budget-item',
   templateUrl: './budget-item.component.html',
   styleUrls: ['./budget-item.component.css']
 })
-export class BudgetItemComponent{
-
+export class BudgetItemComponent implements OnInit{
+  //bound properties to toggle button dropdowns with unique attribute values, set uniquely at ngOnInit()
+  target: string = "item-collapse";
+  bstarget: string = "";
   @Input() item!: BudgetItemDto;
 
+  ngOnInit(): void {
+    this.target += this.item.id.toString();
+    this.bstarget = "#" + this.target;
+  }
 }
