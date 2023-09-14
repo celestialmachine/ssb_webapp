af1837c49b435ae0cf3b010cd9276c16b8ffcac1
Victor Diaz <master_v12@hotmail.com>
2023-09-13 22:15:10 -0700

    Item creation now also calls api to retrieve new events

--- src/app/components/item-list-components/item-list/item-list.component.ts
+++ src/app/components/item-list-components/item-list/item-list.component.ts
@@ -21,20 +21,27 @@ export class ItemListComponent implements OnInit{
     this.api.getItems().subscribe(items => this.items = items);
   }
 
+  addItem(value: BudgetItemDto): void {
+    this.api.addItem(value).subscribe({
+      next: item => this.recieveNewItem(item),
+      error: err => console.log(err),
+      complete: () => console.log("addItem() completed")
+    });
+  }
+
+  //adds newly created budget item to list of items
+  //& calls api to retrieve new budget events
+  recieveNewItem(item: BudgetItemDto): void {
+    this.items.push(item);
+    this.getEventsById(item.itemId as number);
+  }
   getEventsById(eventId: number): void {
     this.api.addEventsByItem(eventId);
   }
 
-  updateItem(value: BudgetItemDto) {
+  updateItem(value: BudgetItemDto): void {
     this.api.updateItem(value).subscribe();
   }
-
-  addItem(value: BudgetItemDto) {
-    this.api.addItem(value).subscribe(item => this.items.push(item));
-    //need to get new item id to call getEventsById from api service
-  }
-
- 
 
   deleteItem(value: BudgetItemDto) {
     //TODO: This is in the wrong order, item should be removed from the list after the api call returns successful deletion.
